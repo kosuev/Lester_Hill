61ed5e2130e680458dda40d5e3348d8cb6c4c0b2
kosuev <kosuev.o@gmail.com>
2016-03-27 19:28:38 +0300

    Створена функція шифрування повідомлення

--- Шифр_Хілла/Program.cs
+++ Шифр_Хілла/Program.cs
@@ -10,19 +10,25 @@ namespace Шифр_Хілла
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your key");
+            Console.WriteLine("Enter your key:");
             string key = Console.ReadLine();
-            Mat(key);
+            Console.WriteLine("Enter your message:");
+            Console.Write("\n");
+            string text = Console.ReadLine();
+            Shifr(key, text);
             Console.ReadKey();
         }
 
-        static void Mat(string key)
+        static void Shifr(string key, string msg)
         {
             int k = key.Length;
-           if (k < 9)                            //доповнюємо повідомлення
+            int[] zt=new int[3];
+            if (k < 9)                            //доповнюємо повідомлення
+            {
                 for (int i = 0; i <= 9 - k; i++)
                     key = key + ".";
-            Console.WriteLine(key);
+                Console.WriteLine("Key: "+key);
+            }
 
             int[,] Mat = new int[3, 3];
             int n = 0;
@@ -34,7 +40,39 @@ namespace Шифр_Хілла
                     if (j == 2)
                         Console.Write("\n");
                     n++;
+                    zt[i]+= Mat[i, j] * msg[j];
                 }
+            Console.WriteLine("\nCrypted text: "+"\n"+Convert.ToChar(zt[0]%127)+ Convert.ToChar(zt[1]%127)+ Convert.ToChar(zt[2]%127));
+            
+                   
         }
+
+        static int Det(string key)
+        {
+            int k = key.Length;
+            if (k < 9)                            //доповнюємо повідомлення
+            {
+                for (int i = 0; i <= 9 - k; i++)
+                    key = key + ".";
+            }
+
+            int[,] Mat = new int[3, 3];
+            int n = 0;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    Mat[i, j] = Convert.ToInt16(key[n]);
+                    Console.Write(Mat[i, j]);
+                    if (j == 2)
+                        Console.Write("\n");
+                    n++;
+                }
+            int det_key = Mat[0, 0] * (Mat[1, 1] * Mat[2, 2] - Mat[1, 2] * Mat[2, 1]) - Mat[0, 1] * (Mat[1, 0] * Mat[2, 2] - Mat[1, 2] * Mat[2, 0]) + Mat[0, 2] * (Mat[1, 0] * Mat[2, 1] - Mat[1, 1] * Mat[2, 0]);
+            while (det_key < 0)
+                det_key += 175;
+            Console.WriteLine(det_key);
+            return det_key;
+        }
+
     }
 }
